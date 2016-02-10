920a31b0d15b1e20ca97bd836e81ed76e36de60e
bronzdoc <lsagastume1990@gmail.com>
2016-02-09 22:18:52 -0600

    Test with access token

--- webhook.rb
+++ webhook.rb
@@ -1,8 +1,17 @@
 require "sinatra"
+require "octokit"
 require "json"
 
-post "/issue/set/triage" do
-  event_json = JSON.parse(params[:payload])
-  event_json.inspect
+configure do
+  Octokit.configure do |o|
+    o.access_token = "02b8d1ede86c8abb1724581822a7663e4c035e38"
+  end
 end
 
+post "/issue/set/triage" do
+  data = JSON.parse(request.body.read)
+  issue_number = data["issue"]["number"]
+  Octokit.add_labels_to_an_issue("bronzdoc/test", issue_number, ["triage"])
+
+  status 200
+end
