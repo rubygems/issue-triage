437658e28b00106e031f13c3a214fcd83c67c40c
bronzdoc <lsagastume1990@gmail.com>
2016-02-10 20:01:40 -0600

    Put stuff in env variables

--- webhook.rb
+++ webhook.rb
@@ -4,14 +4,21 @@ require "json"
 
 configure do
   Octokit.configure do |o|
-    o.access_token = "02b8d1ede86c8abb1724581822a7663e4c035e38"
+    o.access_token = ENV["ACCESS_TOKEN"]
   end
+
+  set :repo,         ENV["REPO"]
+  set :issue_labels, []
+  set :pr_labels,    []
+
+  ENV["ISSUE_LABEL"].split(",").each { |s| settings.issue_labels << s } unless ENV["ISSUE_LABEL"].nil?
+  ENV["PR_LABEL"].split(",").each { |s| settings.pr_labels << s } unless ENV["PR_LABEL"].nil?
 end
 
 post "/issue/set/triage" do
   data = JSON.parse(request.body.read)
   issue_number = data["issue"]["number"]
-  Octokit.add_labels_to_an_issue("bronzdoc/test", issue_number, ["triage"])
+  Octokit.add_labels_to_an_issue(settings.repo, issue_number, settings.issue_labels)
 
   status 200
 end
