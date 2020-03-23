0989570beb7bb71c24648d5b2f692d9a4c32cdb4
bronzdoc <lsagastume1990@gmail.com>
2020-03-23 07:47:53 -0600

    Fix typos, add correct Bundler label

--- webhook.rb
+++ webhook.rb
@@ -19,9 +19,9 @@ module Webhook
       if data["action"] == "opened"
         Octokit.add_labels_to_an_issue(ENV["REPO"], issue_number, [ ENV["ISSUE_LABEL"] ])
         if data.has_key?("pull_request")
-          files = Octokit.pull_request_files
+          files = Webhook.pull_request_files(issue_number)
           if files.any?(/bundler\//)
-            Octokit.add_labels_to_an_issue(ENV["REPO"], issue_number, [ "bundler" ])
+            Octokit.add_labels_to_an_issue(ENV["REPO"], issue_number, [ "Bundler" ])
           end
         end
       elsif data["action"] == "closed"
@@ -32,7 +32,7 @@ module Webhook
     end
   end
 
-  def self.pull_rquest_files(pr_number)
+  def self.pull_request_files(pr_number)
     Octokit.pull_request_files("rubygems/rubygems", pr_number).map {|data| data.filename}
   end
 
